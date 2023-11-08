a1cee03d3492a1eade3c6bfdc24d730a81fd7522
Emil215p <juelemil@gmail.com>
2023-11-08 08:48:45 +0100

    Fully working currency converter that takes info from the Danish national bank.

--- WPFOneThatIsBetter/MainWindow.xaml.cs
+++ WPFOneThatIsBetter/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Input;
+using System.Xml;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace WPFOneThatIsBetter
 {
@@ -13,20 +18,84 @@ namespace WPFOneThatIsBetter
             ClearControls();
             BindCurrency();
         }
+
+        public void ReadXML()
+        {
+            //XmlDocument doc = new XmlDocument();
+            //doc.Load("https://www.nationalbanken.dk/api/currencyratesxml?lang=da");
+
+            //XmlNodeList codeNodes = doc.SelectNodes("//currency/@code");
+            //XmlNodeList rateNodes = doc.SelectNodes("//currency/@rate");
+
+            //string[] codes = new string[codeNodes.Count];
+            //double[] rates = new double[rateNodes.Count];
+
+            //for (int i = 0; i < codeNodes.Count; i++)
+            //{
+            //    codes[i] = codeNodes[i].Value;
+            //    rates[i] = double.Parse(rateNodes[i].Value);
+            //}
+
+            // Now you have the codes and rates in arrays. You can print them out to verify.
+            //for (int i = 0; i < codes.Length; i++)
+            //{
+            //    Console.WriteLine("Code: " + codes[i] + ", Rate: " + rates[i]);
+            //}
+        }
         private void BindCurrency()
         {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("https://www.nationalbanken.dk/api/currencyratesxml?lang=da");
+
+            XmlNodeList codeNodes = doc.SelectNodes("//currency/@code");
+            XmlNodeList rateNodes = doc.SelectNodes("//currency/@rate");
+
+            string[] codes = new string[codeNodes.Count];
+            double[] rates = new double[rateNodes.Count];
+
+            for (int i = 0; i < codeNodes.Count; i++)
+            {
+                codes[i] = codeNodes[i].Value;
+                rates[i] = double.Parse(rateNodes[i].Value);
+            }
+
             DataTable dtCurrency = new DataTable();
             dtCurrency.Columns.Add("Text");
             dtCurrency.Columns.Add("Value");
 
-            //will be replaced by XML soon enough.
-            dtCurrency.Rows.Add("SELECT", 0);
-            dtCurrency.Rows.Add("INR", 1);
-            dtCurrency.Rows.Add("USD", 75);
-            dtCurrency.Rows.Add("EUR", 85);
-            dtCurrency.Rows.Add("SAR", 20);
-            dtCurrency.Rows.Add("POUND", 5);
-            dtCurrency.Rows.Add("DEM", 43);
+            dtCurrency.Rows.Add("SELECT", "SELECT");
+            dtCurrency.Rows.Add(codes[0], rates[0]);
+            dtCurrency.Rows.Add(codes[1], rates[1]);
+            dtCurrency.Rows.Add(codes[2], rates[2]);
+            dtCurrency.Rows.Add(codes[3], rates[3]);
+            dtCurrency.Rows.Add(codes[4], rates[4]);
+            dtCurrency.Rows.Add(codes[5], rates[5]);
+            dtCurrency.Rows.Add(codes[6], rates[6]);
+            dtCurrency.Rows.Add(codes[7], rates[7]);
+            dtCurrency.Rows.Add(codes[8], rates[8]);
+            dtCurrency.Rows.Add(codes[9], rates[9]);
+            dtCurrency.Rows.Add(codes[10], rates[10]);
+            dtCurrency.Rows.Add(codes[11], rates[11]);
+            dtCurrency.Rows.Add(codes[12], rates[12]);
+            dtCurrency.Rows.Add(codes[13], rates[13]);
+            dtCurrency.Rows.Add(codes[14], rates[14]);
+            dtCurrency.Rows.Add(codes[15], rates[15]);
+            dtCurrency.Rows.Add(codes[16], rates[16]);
+            dtCurrency.Rows.Add(codes[17], rates[17]);
+            dtCurrency.Rows.Add(codes[18], rates[18]);
+            dtCurrency.Rows.Add(codes[19], rates[19]);
+            dtCurrency.Rows.Add(codes[20], rates[20]);
+            dtCurrency.Rows.Add(codes[21], rates[21]);
+            dtCurrency.Rows.Add(codes[22], rates[22]);
+            dtCurrency.Rows.Add(codes[23], rates[23]);
+            dtCurrency.Rows.Add(codes[24], rates[24]);
+            dtCurrency.Rows.Add(codes[25], rates[25]);
+            dtCurrency.Rows.Add(codes[26], rates[26]);
+            dtCurrency.Rows.Add(codes[27], rates[27]);
+            dtCurrency.Rows.Add(codes[28], rates[28]);
+            dtCurrency.Rows.Add(codes[29], rates[29]);
+            dtCurrency.Rows.Add(codes[30], rates[30]);
+            dtCurrency.Rows.Add(codes[31], rates[31]);
 
             cmbFromCurrency.ItemsSource = dtCurrency.DefaultView;
             cmbFromCurrency.DisplayMemberPath = "Text";
